816ec051d15ad00184711cc932478da14aab5950
DESKTOP-C38II0I\gerwy <gerwyn@hotmail.com>
2018-03-01 09:31:55 +0000

    S2, L11

--- src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'browser-event-experiments',
-  templateUrl: './browser-event-experiments.component.html',
-  styleUrls: ['./browser-event-experiments.component.css']
+    selector: 'browser-event-experiments',
+    templateUrl: './browser-event-experiments.component.html',
+    styleUrls: ['./browser-event-experiments.component.css']
 })
 export class BrowserEventExperimentsComponent implements OnInit {
 
-  constructor() { }
+    hoverSection: HTMLElement;
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+        this.hoverSection = document.getElementById('hover');
+        this.hoverSection.addEventListener('mousemove', this.onMouseMove);
+    }
+
+    unsubscribe() {
+        console.log('Called unsubsribe()');
+        this.hoverSection.removeEventListener('mousemove', this.onMouseMove);
+    }
+
+    onMouseMove(ev: MouseEvent) {
+        console.log(ev);
+    }
 }
